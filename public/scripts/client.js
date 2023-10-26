c06e0dcfb5eca2ee966b46229ae7e4d1f4750ea2
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-10-26 00:43:28 -0400

    Add to client.js and index.html to display validating error

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,19 @@
 
 $(document).ready(function() {
 
+  $(document).ready(function() {
+    const $errorContainer = $('.error-message');
+    $errorContainer.hide();
+  
+    const showError = function(message) {
+      $errorContainer.html('&#9888; ' + message + ' &#9888;');
+      $errorContainer.slideDown();
+    };
+  
+    const hideError = function() {
+      $errorContainer.slideUp();
+    };
+
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
@@ -60,13 +73,14 @@ $(document).ready(function() {
   const $form = $('.tweet-form');
   $form.submit(function(event) {
     event.preventDefault();
+    hideError();
     const tweetContent = $('#tweet-text').val();
     if (!tweetContent) {
-      alert('Tweet content cannot be empty.');
+      showError('Tweet content cannot be empty.');
       return;
     }
     if (tweetContent.length > 140) {
-      alert('Tweet content exceeds the character limit.');
+      showError('Tweet content exceeds the character limit.');
       return;
     }
     const formData = $(this).serialize();
@@ -88,92 +102,7 @@ $(document).ready(function() {
 
   
 });
+});
 
 
-
-
-
-
-
-
-
-
-// $(document).ready(function() {
-
-// const data =[]
-
-  
-  
-
-// // Function to create a tweet element
-// const createTweetElement = function(tweetData) {
-//   const $tweet = $(`
-//   <article class="tweet">
-//   <header>
-//   <img src="${tweetData.user.avatars}" alt="User Avatar">
-//   <h3 class="user-name">${tweetData.user.name}</h3>
-//   <span class="user-handle">${tweetData.user.handle}</span>
-//   </header>
-//   <p class="tweet-content">${tweetData.content.text}</p>
-//   <footer>
-//   <span class="tweet-timestamp">${timeago.format(tweetData.created_at)}</span>
-//   <div class="tweet-actions">
-//   <i class="far fa-flag"></i>
-//           <i class="far fa-heart"></i>
-//           <i class="fas fa-retweet"></i>
-//         </div>
-//       </footer>
-//     </article>
-//   `);
-
-//   return $tweet;
-// };
-// // Function to render tweets
-// const renderTweets = function(tweets) {
-// for (let tweet of tweets) {
-//   const $tweet = createTweetElement(tweet);
-//   $('#tweets-container').append($tweet);
-// }
-// };
-
-// renderTweets(data);
-
-// const loadTweets = function() {
-//   $.ajax({
-//     url: '/tweets',
-//     method: 'GET',
-//     dataType: 'json',
-//     success: function(data) {
-//       renderTweets(data);
-//     },
-//     error: function(error) {
-//       console.error('Error fetching tweets:', error);
-//     }
-//   });
-// };
-
-// loadTweets();
-// // Event listener for form submission
-// $('form').submit(function(event) {
-//   event.preventDefault(); // Prevent the default form submission
-
-//   const serializedData = $(this).serialize(); // Serialize the form data
-
-//   // AJAX POST request to send the serialized data to the server
-//   $.ajax({
-//     url: '/tweets',
-//     method: 'POST',
-//     data: serializedData
-//   })
-//     .done(function(response) {
-//       // Handle the successful response
-//       console.log('Data sent to the server successfully!', response);
-//       console.log('data', data);
-//     })
-//     .fail(function(error) {
-//       // Handle any errors that occur
-//       console.error('Error sending data to the server!', error);
-//     });
-// });
-// });
 
